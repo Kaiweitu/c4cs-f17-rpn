871820a7c51bfe0adcd49daf13ee9460368fa0d2
Kaiwei Tu <kaiweitu@umich.edu>
2017-10-28 00:39:01 +0800

    rpn calculator

---
+++ rpn.py
@@ -0,0 +1,27 @@
+import operator
+
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            stack.append(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            result = function(arg1,arg2)
+            stack.append(result)
+    print(stack)
+    return stack.pop()
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()
