f80d5894a189079d76e8da95401e6c95c100de0f
Kaiwei Tu <kaiweitu@umich.edu>
2017-11-04 09:29:28 +0800

    Modify the rpn

--- rpn.py
+++ rpn.py
@@ -3,6 +3,8 @@ import operator
 ops = {
     '+': operator.add,
     '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
 def calculate(myarg):
@@ -17,11 +19,14 @@ def calculate(myarg):
             result = function(arg1,arg2)
             stack.append(result)
     print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
