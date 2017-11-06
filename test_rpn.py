0e5b76f19ddae7cdace751f9e6990abb45db62c5
Kaiwei Tu <kaiweitu@umich.edu>
2017-11-07 02:34:02 +0800

    Modify test_rpn

--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,9 @@ class TestBasics(unittest.TestCase):
     def test_exponentiation(self):
         result = rpn.calculate("2 3 ^")
         self.assertEqual(8,result)
+    def test_lshift(self):
+        result = rpn.calculate("2 3 <<")
+        self.assertEqual(16,result)
+
+if __name__ == '__main__':
+    unittest.main()
